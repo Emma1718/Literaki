d5fbcdd95238583a84026d89a6aa1968c5413c1c
Paulina Jankowska <paulinkaj1@gmail.com>
2012-10-28 17:02:36 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ int main(int argc, char * argv[])
 {
   Game game(argc, argv);
   game.run();
-  return 0;
   // Dictionary dict;
   // dict.load("plik.txt");
   // dict.checkWord("ktury"); //false
   // dict.checkWord("ser") //true
+  return 0;
 }
